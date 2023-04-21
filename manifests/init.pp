8a7423b7d34fd880e2bc835a3c2da593e776e6b8
Jake Rundall <rundall@illinois.edu>
2023-04-21 17:06:00 -0500

    SVCPLAN-2644: update dnfmodule syntax to avoid looping
    This profile kept re-enabling the managed dnf modules on
    each Puppet run.
    
    Borrowing syntax from https://github.com/osc/puppet-module-openondemand
    to stop this.
    
    Also:
    - pdk update
    - pdk validate / corrections

--- manifests/init.pp
+++ manifests/init.pp
@@ -8,44 +8,44 @@ class profile_ondemand (
   String $nodejs_version,
   String $ruby_version,
   Hash $crons,
-){
+) {
+  include apache::mod::rewrite
+  include apache::mod::env
+  include apache::mod::alias
+  include apache::mod::authn_core
+  include apache::mod::authz_user
+  include openondemand
 
-  include ::apache::mod::rewrite
-  include ::apache::mod::env
-  include ::apache::mod::alias
-  include ::apache::mod::authn_core
-  include ::apache::mod::authz_user
-  include ::openondemand
-
-  package { "nodejs:${nodejs_version}":
-    ensure => 'present',
-    provider => 'dnfmodule',
-    before => Class['openondemand']
+  package { 'nodejs':
+    ensure      => $nodejs_version,
+    enable_only => true,
+    provider    => 'dnfmodule',
+    before      => Class['openondemand'],
   }
 
-  package { "ruby:${ruby_version}":
-    ensure => 'present',
-    provider => 'dnfmodule',
-    before => Class['openondemand']
+  package { 'ruby':
+    ensure      => $ruby_version,
+    enable_only => true,
+    provider    => 'dnfmodule',
+    before      => Class['openondemand'],
   }
 
-  file { "/etc/ood/config/ood-portal.conf":
-    mode => '0600'
+  file { '/etc/ood/config/ood-portal.conf':
+    mode => '0600',
   }
 
-  file { "/opt/ood/custom":
+  file { '/opt/ood/custom':
     ensure => 'directory',
-    mode => '0755'
+    mode   => '0755',
   }
 
-  file { "/opt/ood/custom/ood-gridmap.py":
-    ensure => 'present',
-    mode => '0755',
-    content => file("profile_ondemand/ood-gridmap.py")
+  file { '/opt/ood/custom/ood-gridmap.py':
+    ensure  => 'file',
+    mode    => '0755',
+    content => file('profile_ondemand/ood-gridmap.py'),
   }
 
   $crons.each | $k, $v | {
     cron { $k: * => $v }
   }
-
 }
