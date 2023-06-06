18701060aab6637224540ddb8e13dc2d8d426090
Yan Zhan <yanzhan2@illinois.edu>
2023-06-06 09:08:53 -0500

    HELMAG-5836 Add ability to have favorite path shortcuts in the Files app

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,16 @@
-# @summary A short summary of the purpose of this class
+# @summary Main class for profile_ondemand
 #
-# A description of what this class does
+# @param nodejs_version
+#   The Node.js version to use for dependency
+#
+# @param ruby_version
+#   The Ruby version to use for dependency
+#
+# @param crons
+#   Hash of cron jobs to set up
+#
+# @param favorite_paths
+#   Array of paths to be added as favorites for the Files app
 #
 # @example
 #   include profile_ondemand
@@ -8,6 +18,7 @@ class profile_ondemand (
   String $nodejs_version,
   String $ruby_version,
   Hash $crons,
+  Array $favorite_paths,
 ) {
   include apache::mod::rewrite
   include apache::mod::env
@@ -43,6 +54,20 @@ class profile_ondemand (
     ensure  => 'file',
     mode    => '0755',
     content => file('profile_ondemand/ood-gridmap.py'),
+  }
+
+  exec { 'mkdir -p /etc/ood/config/apps/dashboard/initializers':
+    creates => '/etc/ood/config/apps/dashboard/initializers',
+    path    => ['/usr/bin', '/usr/sbin'],
+    # Need this to execute after the openondemand module creates and cleans out the directory
+    require => File['/etc/ood/config/apps'],
+  }
+
+  file { '/etc/ood/config/apps/dashboard/initializers/ood.rb':
+    ensure  => 'file',
+    mode    => '0755',
+    content => epp('profile_ondemand/apps/dashboard/initializers/ood.rb.epp'),
+    require => Exec['mkdir -p /etc/ood/config/apps/dashboard/initializers'],
   }
 
   $crons.each | $k, $v | {
